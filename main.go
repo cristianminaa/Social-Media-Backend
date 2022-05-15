42ed4c839ffb1fd35c01cc7db0d23eba428dc092
Cristian Mina <32262469+cristianminaa@users.noreply.github.com>
2022-05-15 16:25:33 +0300

    Basic backend server + files

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Server listening at localhost:8080\n")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	server := http.Server{
+		Handler:      mux,
+		Addr:         "localhost:8080",
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+	server.ListenAndServe()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write([]byte("{}"))
+}
