b43ee5f6fe8ca113689b5ec3f8c7475936eb9fe7
Cristian Mina <32262469+cristianminaa@users.noreply.github.com>
2022-05-15 18:40:50 +0300

    added error handling

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"Social-Media-Backend/internal/database"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +14,7 @@ func main() {
 	fmt.Printf("Server listening at localhost:8080\n")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/err", errHandler)
 	server := http.Server{
 		Handler:      mux,
 		Addr:         "localhost:8080",
@@ -22,7 +26,43 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	respondWithJSON(w, 200, database.User{
+		Email: "test@example.com",
+	})
+}
+
+func errHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, 500, errors.New("internal server error"))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE")
+	if payload != nil {
+		response, err := json.Marshal(payload)
+		if err != nil {
+			w.WriteHeader(500)
+			response, _ := json.Marshal(errorBody{
+				Error: "Error marshalling JSON",
+			})
+			w.Write(response)
+			return
+		}
+		w.WriteHeader(code)
+		w.Write(response)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	if err != nil {
+		log.Println(err)
+		respondWithJSON(w, code, errorBody{
+			Error: err.Error(),
+		})
+	}
+	log.Println("don't call respondWithError with no error")
+}
+
+type errorBody struct {
+	Error string `json:"error"`
 }
