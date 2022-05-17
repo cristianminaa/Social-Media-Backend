c106459e1ffcffb2b065cf4bd1fbfb2897c3a111
Cristian Mina <32262469+cristianminaa@users.noreply.github.com>
2022-05-17 09:53:06 +0300

    adding handlers for local JSON db

---
+++ handlers.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// calling handlers depending on the method
+func (apiCfg apiConfig) endpointUsersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// call GET handler
+	case http.MethodPost:
+		// call POST handler
+	case http.MethodPut:
+		// call PUT handler
+	case http.MethodDelete:
+		// call DELETE handler
+	default:
+		respondWithError(w, 404, errors.New("method not supported"))
+	}
+}
+
+func (apiCfg apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	userEmail := strings.TrimPrefix(r.URL.Path, "/users/")
+	if userEmail == "" {
+		respondWithError(w, http.StatusBadRequest, errors.New("no userEmail"))
+		return
+	}
+	user, err := apiCfg.dbClient.GetUser(userEmail)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, user)
+}
+
+func (apiCfg apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+		Age      int    `json:"age"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	user, err := apiCfg.dbClient.CreateUser(params.Email, params.Password, params.Name, params.Age)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
+func (apiCfg apiConfig) handlerUpdateUser(w http.ResponseWriter, r *http.Request) {
+	userEmail := strings.TrimPrefix(r.URL.Path, "/users/")
+	type parameters struct {
+		Password string `json:"password"`
+		Name     string `json:"name"`
+		Age      int    `json:"age"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	// throw error if params are not provided or if they cannot be decoded
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// throw error if there is no email provided
+	if userEmail == "" {
+		respondWithError(w, http.StatusBadRequest, errors.New("no email provided"))
+		return
+	}
+	// start updating user
+	newUser, err := apiCfg.dbClient.UpdateUser(userEmail, params.Password, params.Name, params.Age)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, newUser)
+}
+
+func (apiCfg apiConfig) handlerDeleteUser(w http.ResponseWriter, r *http.Request) {
+	userEmail := strings.TrimPrefix(r.URL.Path, "/users/")
+	if userEmail == "" {
+		respondWithError(w, http.StatusBadRequest, errors.New("no userEmail, aborting"))
+		return
+	}
+	err := apiCfg.dbClient.DeleteUser(userEmail)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, struct{}{})
+}
